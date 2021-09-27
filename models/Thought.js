22158dd5c80cdf70ad94956f1816ece24d16a541
Raevin Dunson <raevin.dunson98@gmail.com>
2021-09-26 23:58:16 -0400

    date format updates and thought models

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,16 @@
 const { Schema, model, Types } = require('mongoose');
 
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            length: [1, 280]
+        },
+        createdAt: {
+            
+        }
+    }
+)
+
 module.exports = Thought;
