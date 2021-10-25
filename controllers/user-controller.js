2868bc81cee15bbe0e19f580f3bc6a2140cc5c1b
Raevin Dunson <raevin.dunson98@gmail.com>
2021-10-25 19:49:14 -0400

    controllers and html updates

---
+++ controllers/user-controller.js
@@ -0,0 +1,13 @@
+const { User, Thought } = require("../models");
+
+const userController = {
+    addUser({ params, body }, res)    
+    User.c.then((id) => {
+        return User.find(All)
+    })
+    .then() => 
+        
+    })
+}
+
+module.exports = usercontroller
